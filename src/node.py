26aaa498146579d1b98ef572c36191961c095593
ghoshanjega <gjaa@connect.ust.hk>
2018-12-26 23:38:31 +0800

    fib is a dict with node objects mapped to some value

--- src/node.py
+++ src/node.py
@@ -8,22 +8,23 @@ class Node:
         self.content_store = ContentStore(size)
         self.fib = fib
         
-    def forward(self, pkt):
-        # not sure what this is
+    def forward(self, pkt, src):
+        for node in fib:
+            node.recieve(pkt,src)
         
 
     def get_gateway():
         return
 
 
-    def recv(self, pkt: Packet, src):
+    def recieve(self, pkt: Packet, src):
         pkt.hopcount+=1 
         if pkt.is_interest:
             found = self.content_store.has(pkt)
             if found in not None:
                 return found
             else:
-                #forward interest packet to next node?
+                forward(pkt, src) #TODO add interest packet adn src to PIT
         else:
             #TODO to be implemented if PIT
             print("data packet")
