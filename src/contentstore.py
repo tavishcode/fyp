2c432f42ae9d56c57b5fbacff44776cd5a9a22d7
ghoshanjega <gjaa@connect.ust.hk>
2019-01-06 13:25:45 +0800

    CS updated with sets. also to get gateway router

--- src/contentstore.py
+++ src/contentstore.py
@@ -1,32 +1,28 @@
-# TODO update with sets
 from packet import Packet
 
 class ContentStore:
     def __init__(self,space):
         self.SIZE = space
-        self.store = [None] * self.SIZE 
+        self.store = set()
 
     def available_space(self):
-        available = self.space
-        for i in range(len(self.store)):
-            if self.store[i] is not None:
-                available = available-1
+        available = self.SIZE - len(self.store)
         return available, self.SIZE
 
     def add_item(self, item):
-        self.store.remove()
-        self.store.append(item)
-        return
+        if(len(self.store)<self.SIZE):
+            self.store.add(item)
+        else:
+            self.store.pop() # TODO: Fancy way to discard a packet
+            self.store.add(item) 
 
     def empty_store(self):
-        self.store = [None] * self.SIZE
-        return
+        self.store = self.store.clear()
 
     def has(self,pkt: Packet):
         for item in self.store:
-            while item is not None:
-                if item.get_name() == pkt.get_name():
-                    return item
+            if item.get_name() == pkt.get_name():
+                return item
         return None
 
 
--- src/node.py
+++ src/node.py
@@ -2,18 +2,31 @@ from contentstore import ContentStore
 from packet import Packet
 
 
+
 class Node:
     def __init__(self, fib, size):
         self.CACHESIZE = size
         self.content_store = ContentStore(size)
-        self.FIB = fib
-        self.PIT = {}
-        
+        self.FIB = fib # a dict "packet name" : node
+        self.PIT = {} # a dict with "packet name" : src node
+    
+    def print_fib(self):
+        print "FIB of node"
+        for item in self.FIB.items():
+            print item
+
+    def print_pit(self):
+        print "PIT of node"
+        for item in self.PIT.items():
+            print item
+
     def forward(self, pkt: Packet, src):
         self.FIB[pkt].receive(pkt, src)
         
-    def get_gateway(self):
-        return
+    def get_gateway(self): 
+        if (len(set(self.FIB.values())) == 1):
+            return set(self.FIB.values())
+        return False
 
     def receive(self, pkt: Packet, src):
         pkt.hopcount+=1 
