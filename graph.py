901d7296e34ed2bb05ff861b623dbc771a206125
akjadoon <asfandyarkjadoon@gmail.com>
2019-01-19 15:10:35 +0800

    Docs + formatting

--- graph.py
+++ graph.py
@@ -3,18 +3,32 @@ sys.path.insert(0, './src')
 from router import Router
 import random 
 
+"""
+    Container class for router nodes
+    Generates a Grid-Based network topology and sets routes for nodes
+    
+    Args: 
+        cache_size: capacity of a router node cache
+        grid_rows: no of rows in router grid  
+        grid_cols: no of columns in router grid
+    Attributes:
+        routers: list of all router objects 
+        num_routers: no of routers in routers
+        adj_mtx: adjacency matrix that models connections between router nodes
+        
+"""
 class Graph:
     def __init__(self, cache_size, grid_rows, grid_cols):
-
-        # init grid topology matrix for routers
-        self.num_routers = grid_rows * grid_cols
+        """Creates routers and sets node links in adj_mtx"""
         self.routers = []
-
+        self.num_routers = grid_rows * grid_cols
+        
         # init routers
         for i in range(0, self.num_routers):
-            name = 'r' + str(i) 
+            name = 'r' + str(i)
             self.routers.append(Router(cache_size, name)) 
 
+        # set adjacency matrix 
         self.adj_mtx = []
 
         for i in range(self.num_routers):
@@ -24,8 +38,7 @@ class Graph:
                     self.adj_mtx[-1].append(1)
                 else:
                     self.adj_mtx[-1].append(0)
-        
-        # connect routers in a grid
+
         for i in range(self.num_routers):
             row_ix = i // grid_cols % grid_rows
             col_ix = i % grid_cols
@@ -34,50 +47,22 @@ class Graph:
                 pair_col_ix = j % grid_cols
                 if abs(pair_row_ix - row_ix) + abs(pair_col_ix - col_ix) == 1:
                     self.adj_mtx[i][j] = 1
-
-    def getRouterByName(self, name):
-        for r in self.routers:
-            if r.name == name:
-                return r 
     
     def get_random_router(self):
         return random.choice(self.routers)
-    
-    def getNeighbors(self):
-        pass
 
-    def getNumRouters(self):
-        return self.num_routers
-
-    def getRouterNames(self):
-        return ['r' + str(i) for i in range(0, self.num_routers)]
-
-    # set fib for routers
-    def setRoutesToProducers(self, producers): 
-        for r in self.routers:
-            fib = {}
-            for p in producers:
-                if r == p.gateway:
-                    fib[p.content] = p
-                else:
-                    fib[p.content] = self.get_best_hop(self.adj_mtx, self.getPos(r), self.getPos(p.gateway))
-            r.FIB = fib
-
-    def getPos(self, router):
+    def get_pos(self, router):
+        """Returns position of router in grid 
+        where pos / grid_rows = row and pos % grid_cols = col"""
         return int(router.name.split('r')[1])
 
-    def getNthRouter(self, n):
-        pass
-
     def get_shortest_path(self, mtx, src, dest):
-        # print("DEST: " + str(dest) + " TYPE: " + str(type(dest)))
+        """Returns list of node pos for shortest path from src to dest"""
         visited = set()
         q = [[src]]
         while q:
             path = q.pop(0)
-            # print("PATH: " + str(path))
             front = path[-1]
-            # print("FRONT: " + str(front))
             visited.add(front)
             if front == dest:
                 return path
@@ -88,6 +73,21 @@ class Graph:
                     q.append(new_path)
 
     def get_best_hop(self, mtx, src, dest):
+        """Returns next router in shortest path from src to dest"""
         interm = self.get_shortest_path(mtx, src, dest)[1]
         result = self.routers[interm]
         return result
+
+    def set_routes_to_producers(self, producers):
+        """Sets FIB for shortest path to producers for all routers"""
+        for r in self.routers:
+            fib = {}
+            for p in producers:
+                if r == p.gateway:
+                    fib[p.content] = p
+                else:
+                    fib[p.content] = self.get_best_hop(self.adj_mtx, self.get_pos(r), self.get_pos(p.gateway))
+            r.FIB = fib
+
+
+
--- src/packet.py
+++ src/packet.py
@@ -1,3 +1,10 @@
+""" A CCN Packet
+
+    Attrributes: 
+        name: String to address pkt
+        hop_count: no of network hops made by pkts so far
+        is_interest: True if pkt is Interest pkt, False if pkt is data pkt
+"""
 class Packet:
     def __init__(self, name, is_interest = True, hop_count = 0):
         self.name = name
