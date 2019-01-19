54729b4d592ae2cd2dcb2222fc2d23988b59ab7f
gjaganathamani <gjaganathamani@sierrawireless.com>
2019-01-19 19:00:19 +0800

    started Keras

--- DLCPP.md
+++ DLCPP.md
@@ -1,6 +1,6 @@
 # DLCPP
 
-## Step 1. **Initializing Phase**
+## Step 1. **Collect data**
 Given training samples X and desired number of hidden layers
 - labeled data at the node
   - amount of all content being being requested at node i at t slot time **(request (t)i )**
@@ -8,8 +8,11 @@ Given training samples X and desired number of hidden layers
   - the entropy of content type at node i during the t slot time **(request_entropy(t)i)**
   - the number of content type at node i during the t slot time **(content_sum(t)i)**
 
-- labeled common data
+- labeled common data (TODO)
   - the amount of all contents being requested by all nodes at t slot time **(request_allnode(t))**
   - the amount of content *o* being requested by all nodes during t slot time **(content_request_allnode(o,t))**
   - post time for t slot **(timestamp)**
 
+Data is colected from each node at **t** time slot. 
+
+##Step2. **Process data on a NN to extract features**
--- test.py
+++ test.py
@@ -4,4 +4,33 @@ from keras.models import Model
 
 df = read_csv("pre-train.csv")
 
-encoding_dim = 32
+encoding_dim = 3 #4 to 3 encoding
+my_epochs = 2 #number of passes
+
+
+# this is the input matrix per node
+input_features = Input(shape=(4,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_features)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(4, activation='relu')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_features, decoded)
+encoder = Model(input_features, encoded)
+
+# placeholder for the encoded data
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(loss='mean_squared_error', optimizer='sgd')
+
+
+
+
+def run(routers,timestamp):
+    for router in routers:
+        
