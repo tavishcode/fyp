d19848d8fbaba1e2f5b92809a60cad0cdf45e4a1
tavish <tavish.gobindram@gmail.com>
2019-01-23 15:54:35 +0800

    sim updates and ddpg contentstore

--- src/contentstore.py
+++ src/contentstore.py
@@ -1,5 +1,16 @@
 from packet import Packet
 from collections import OrderedDict, defaultdict
+import sys
+import numpy as np
+sys.path.insert(0, './ddpg_cache')
+
+"""DDPG-RL Imports"""
+
+from ddpg_cache_train import Trainer as ddpg_trainer
+from ddpg_cache_buffer import MemoryBuffer
+
+"""---------------"""
+
 
 """ Abstract Class for defining Cache Policies.
 
@@ -39,11 +50,6 @@ class FifoContentStore(ContentStore):
         self.store = OrderedDict()
         self.req_hist = defaultdict(int) # for testing purposes
 
-    def update_state(self): # for testing purposes
-        counts = len(self.req_hist)
-        self.req_hist = defaultdict(int)
-        return counts
-
     def add(self, item):
         if self.size:
             if(len(self.store) == self.size):
@@ -76,6 +82,13 @@ class LruContentStore(ContentStore):
             return cached_item
         except:
             return None
+
+    def get(self, item_name):
+        item = self.get_helper(item_name)
+        if item != None:
+            self.hits += 1
+        else:
+            self.misses += 1
 
 """Least Frequently Used Cache Policy"""
 class LfuContentStore(ContentStore):
@@ -121,24 +134,127 @@ class DlcppContentStore(ContentStore):
         self.prev_reqs = self.curr_reqs
         self.curr_reqs = defaultdict(int)
 
-"""DDPG Cache Policy - RL"""
+
+"""DDPG Cache Policy with Bootstrap"""
 class DdpgContentStore(ContentStore):
-    def __init__(self, size):
+    def __init__(self, size, num_content_types):
         super().__init__(size)
+
+        # Bootstrap first updates
+        self.bootstrap = LfuContentStore(size)
+        self.bootstrap_period = 1000000
+
+        self.NUM_CONTENT_TYPES = num_content_types
         self.curr_reqs = defaultdict(int)
-    
+        self.ram = MemoryBuffer(1000000)
+        self.trainer = ddpg_trainer(self.NUM_CONTENT_TYPES, self.NUM_CONTENT_TYPES, self.ram)
+        self.trainer.load_models(1)
+        self.first_run = True
+        self.state = []
+        self.action = None
+        self.num_updates = 0
+        self.model_updates = 0
+        self.temp_hits = 0
+        self.temp_misses = 0
+
     def get_helper(self, item):
         try:
             self.curr_reqs[item.name] += 1
+            cached_item = self.store[item.name]
+            return cached_item
         except:
+            return None
+    
+    def add(self, item):
+        if self.num_updates < self.bootstrap_period:
+            self.bootstrap.add(item) 
+        else:
             pass
 
-        
+    def get(self, item_name):
+        """Wrapper function for get_helper which includes hit/miss statistic updates"""
+        item = self.get_helper(item_name)
+        if item != None:
+            if self.num_updates > self.bootstrap_period:
+                self.hits += 1
+            self.temp_hits += 1
+        else:
+            if self.num_updates > self.bootstrap_period:
+                self.misses += 1
+            self.temp_misses += 1
+        if self.num_updates < self.bootstrap_period: # use bootsrap
+            item = self.bootstrap.get(item_name)
+            self.hits = self.bootstrap.hits
+            self.misses = self.bootstrap.misses
+        return item
 
+    def update_ranking(self):
+        if hasattr(self.action, '__len__'):
+            rating_table = []
+            for ix in range(len(self.action)):
+                rating_table.append(['content'+ str(ix), self.action[ix]])
+            rating_table.sort(key = lambda x: x[1], reverse = True)
+            self.store = {}
+            for ix in range(self.size):
+                self.store[rating_table[ix][0]] = Packet(rating_table[ix][0], is_interest=False)
 
+    def update_state(self):
+        temp_reqs = self.temp_hits + self.temp_misses
 
+        if temp_reqs and not self.first_run:
 
+            reward = self.temp_hits/temp_reqs
+            # print('reward ' + str(reward))
+            new_state = []
+            for c in range(self.NUM_CONTENT_TYPES):
+                new_state.append(0)
+            for key in self.curr_reqs.keys():
+                ix = int(key[-1])
+                new_state[ix] = self.curr_reqs[key]/temp_reqs
 
+            # print('prev state', self.state)
+            # print('new state', new_state)
 
+            if self.num_updates < self.bootstrap_period:
+                # pass
+                self.ram.add(np.float32(self.state), self.action, reward, np.float32(new_state))
+            
+            self.state = new_state
+            
+            if self.num_updates < self.bootstrap_period:
+                # pass
+                self.trainer.optimize()
+                self.action = self.trainer.get_exploration_action(np.float32(self.state))
+                self.model_updates += 1
+                if self.model_updates % 10000 == 0:
+                    self.trainer.save_models(1)
+                    print('saving model after ' + str(self.model_updates) + ' updates and ' + str(self.num_updates) + ' timesteps')
+            else:
+                self.action = self.trainer.get_exploitation_action(np.float32(self.state))
+            # print('next action', self.action)
+            self.update_ranking()
+        elif temp_reqs and self.first_run:
+            self.state = []
+            for c in range(self.NUM_CONTENT_TYPES):
+                self.state.append(0)
+            for key in self.curr_reqs.keys():
+                ix = int(key[-1])
+                self.state[ix] = self.curr_reqs[key]/temp_reqs
 
+            if self.num_updates < self.bootstrap_period:
+                self.action = self.trainer.get_exploration_action(np.float32(self.state))
+                # pass
+            else:
+                self.action = self.trainer.get_exploitation_action(np.float32(self.state))
+            # print('first action', self.action)
+            self.first_run = False
+            self.update_ranking()
 
+        self.temp_hits = 0
+        self.temp_misses = 0
+        self.curr_reqs = defaultdict(int)
+        self.num_updates += 1
+        if self.num_updates % 1000 == 0:
+            print(self.num_updates)
+        if self.num_updates == self.bootstrap_period:
+            self.trainer.load_models(1)
--- src/router.py
+++ src/router.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, './src')
-from contentstore import FifoContentStore, LruContentStore, LfuContentStore
+from contentstore import FifoContentStore, LruContentStore, LfuContentStore, DdpgContentStore
 from packet import Packet
 
 """ A CCN Router Node
@@ -14,13 +14,15 @@ from packet import Packet
 
 """
 class Router:
-    def __init__(self, cache_size, name, policy):
+    def __init__(self, cache_size, num_content_types, name, policy):
         if policy == 'fifo':
             self.contentstore = FifoContentStore(cache_size)
         elif policy == 'lru':
             self.contentstore = LruContentStore(cache_size)
         elif policy == 'lfu':
             self.contentstore = LfuContentStore(cache_size)
+        elif policy == 'ddpg':
+            self.contentstore = DdpgContentStore(cache_size, num_content_types)
         self.FIB = {} 
         self.PIT = {} 
         self.name = name
@@ -49,10 +51,10 @@ class Router:
         """
         pkt.hop_count += 1
         if pkt.is_interest:
-            print(self.name + ' receives request for ' + pkt.name)
+            # print(self.name + ' receives request for ' + pkt.name)
             found = self.contentstore.get(pkt)
             if found != None:
-                print(self.name + ' found ' + pkt.name + ' in cache')
+                # print(self.name + ' found ' + pkt.name + ' in cache')
                 new_data_pkt = Packet(pkt.name, is_interest=False, hop_count=pkt.hop_count)
                 src.q.append({'time': time + 0.1, 'type': 'REC','pkt': new_data_pkt, 'src': self})
                 src.q.sort(key=lambda x: x['time'])
@@ -64,7 +66,7 @@ class Router:
                     self.FIB[pkt.name].q.append({'time': time + 0.1, 'type': 'REC','pkt': pkt,'src': self})
                     self.FIB[pkt.name].q.sort(key=lambda x: x['time'])
         else:
-            print(self.name + ' receives data packet for ' + pkt.name)
+            # print(self.name + ' receives data packet for ' + pkt.name)
             self.contentstore.add(pkt)
             for ix, val in enumerate(self.PIT[pkt.name]):
                 node, hop_count = val
