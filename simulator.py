6fd732f78d9813bb1dc76441a8e14955892303b7
tavish <tavish.gobindram@gmail.com>
2019-03-04 16:49:59 +0800

    updates

--- simulator.py
+++ simulator.py
@@ -61,7 +61,6 @@ class Simulator:
         self.rng = np.random.RandomState(self.RAND_SEED)                # random number generator responsible for all non third-party randomizations
 
 
-        # self.q = EventList()                                            # global queue of events
         self.prev_cache_update = 0                                      
         self.curr_time = 0
         self.curr_day = 0                                              # continuously increasing time
