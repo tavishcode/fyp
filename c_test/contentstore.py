52e6c3c1ef01e55609c4874b37486f034cf791ee
tavish <tavish.gobindram@gmail.com>
2019-04-22 02:20:55 +0800

    update protocol and cleaned files

--- c_test/contentstore.py
+++ c_test/contentstore.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 
+
 class ContentStore:
     def __init__(self, size):
         self.size = size
@@ -13,6 +14,8 @@ class ContentStore:
         pass
 
     def get_helper(self, item):
+        # function is overriden for custom get implementations
+        # in derived classes
         pass
 
     def get(self, item):
@@ -23,12 +26,6 @@ class ContentStore:
             self.misses += 1
         return item
 
-    def has(self, item):
-        if item in self.store:
-            return self.store[item]
-        else:
-            return None
-
 
 class LruContentStore(ContentStore):
     def __init__(self, size):
@@ -36,18 +33,21 @@ class LruContentStore(ContentStore):
         self.store = OrderedDict()
 
     def add(self, item):
+        # returns can cache ?, victim
         victim = None
-        if item in self.store:
+        if item in self.store or self.size <= 0:
             return False, victim
         if self.size:
             if(len(self.store) == self.size):
+                # remove top element (least recent element)
                 victim, _ = self.store.popitem(last=False)
             self.store[item] = item
-        return True, victim
+            return True, victim
 
     def get_helper(self, item):
         try:
             cached_item = self.store.pop(item)
+            # re-insert content to maintain lru-order
             self.store[item] = cached_item
             return cached_item
         except:
@@ -55,7 +55,7 @@ class LruContentStore(ContentStore):
 
 
 class PretrainedCNNContentStore(ContentStore):
-    def __init__(self, size, model, online=False):
+    def __init__(self, size):
         super().__init__(size)
 
         # bootstrapping
@@ -64,20 +64,17 @@ class PretrainedCNNContentStore(ContentStore):
         self.bootstrapping = True
 
         # constants
-        self.num_features = 1
         self.pred_length = 7
         self.window_length = 7
         self.num_features = 11
-        self.reqs_per_day = 50000
+        self.reqs_per_day = 500000
+        self.num_portals = 10
 
         # counter
-        self.timestep = 0
         self.req_counter = 0
 
         # ml-related
-        # self.model = load_model(
-        #     '../trained_models/improved_simple_conv_with_portals.h5')
-        self.model = model
+        self.model = load_model('../trained_models/opt_reshaped_simple_conv_with_portals.h5')
         self.scaler = MinMaxScaler()
 
         # stats
@@ -87,33 +84,44 @@ class PretrainedCNNContentStore(ContentStore):
         self.ranking = OrderedDict()
 
         # cache
-        self.store = {}
+        self.store = OrderedDict()
 
         # data
-        portals_arr = np.load('../portals.npy')
-        self.portals = {}
-        for i in range(portals_arr.shape[0]):
-            self.portals[str(i)] = portals_arr[i]
+        self.portals = np.load('../portals_dict.npy').item()
+    
+    def get_curr_day(self):
+        return (self.req_counter - 1) // self.reqs_per_day
+    
+    def get_curr_timestep(self):
+        return self.get_curr_day() % self.pred_length
 
+    def get_portal_key(self, item):
+        # extract index from item name ex: from serverA/12 to 12
+        if isinstance(item, str):
+            start_ix = item.rfind('/') + 1
+            return int(item[start_ix:])
+        else:
+            return item
+    
     # add item to cache if more popular
     def add(self, item):
         victim = None
         if self.bootstrapping:
             return self.bootstrap.add(item)
         else:
-            if item in self.store:
+            if item in self.store or self.size <= 0:
                 return False, victim
             if self.size:
                 if len(self.store) == self.size:
                     min_key, min_rank = self.get_least_popular()
-                    timestep = self.req_counter // self.reqs_per_day % self.pred_length
                     # replace if curr item more popular than least popular in cache
-                    if min_rank != None and min_rank < self.ranking[item][timestep]:
+                    if min_rank != None and min_rank < self.ranking[item][self.get_curr_timestep()]:
                         victim = min_key
                         self.store.pop(victim)
                         self.store[item] = item
                         return True, victim
-                    return False, victim
+                    else:
+                        return False, victim
                 else:
                     self.store[item] = item
                     return True, victim
@@ -122,58 +130,69 @@ class PretrainedCNNContentStore(ContentStore):
         min_item = None
         min_rank = None
         for item in self.store.keys():
-            timestep = self.req_counter // self.reqs_per_day % self.pred_length
-            if min_item == None or self.ranking[item][timestep] < min_rank:
-                min_rank = self.ranking[item]
+            if min_item == None or self.ranking[item][self.get_curr_timestep()] < min_rank:
+                min_rank = self.ranking[item][self.get_curr_timestep()]
                 min_item = item
         return min_item, min_rank
 
     def update_rankings(self):
-        self.timestep += 1
-        if not self.bootstrapping:
-            # init npy arrays
-            agg_data = np.zeros((len(self.history.keys()), self.window_length))
-            portal_data = np.zeros((last_week.shape[0], self.window_length, 10))
+        self.bootstrapping = False
+        
+        # init npy arrays
+        agg_data = np.zeros((len(self.history.keys()), self.window_length))
+        portal_data = np.zeros((len(self.history.keys()), self.window_length, self.num_portals))
+                
+        for ix, key in enumerate(self.history.keys()):
+            agg_data[ix] = self.history[key]
+            portal_key = self.get_portal_key(key)
+            if portal_key in self.portals:
+                portal_encoding = np.tile(self.portals[portal_key], (self.window_length, 1))
+            else:
+                portal_encoding = np.tile(np.zeros((self.num_portals)), (self.window_length, 1))
+            portal_data[ix] = portal_encoding
+             
+        # log and normalize data
+        agg_data = np.log1p(agg_data)
+        agg_data = self.scaler.fit_transform(agg_data)
+        
+        agg_data = agg_data.reshape(-1, self.window_length, 1)
 
-            # reshape data
-            for i, item in enumerate(self.history.keys()):
-                agg_data[i] = self.history[item]
-                if item in self.portals:
-                    portal_data[item] = np.tile(self.portals[item], (self.window_length, 1))
-                else:
-                    portal_data[item] = np.tile(np.zeros(10), (self.window_length, 1))
+        # add portal features
+        agg_data = np.concatenate((agg_data, portal_data), axis=2)
+        
+        # make preds
 
-            # log and normalize data
-            agg_data = np.log1p(agg_data)
-            agg_data = self.scaler.fit_transform(agg_data)
-            agg_data = agg_data.reshape(-1, 7, 1)
+        predictions = self.predict_sequence(agg_data)  # update rankings                
+        rankings = predictions[:, :, 0]
+        
+        # reset old ranking
+        self.ranking = OrderedDict()
 
-            # add portal features
-            agg_data = np.concatenate((agg_data, portal_data), axis=2)
-
-            # make preds
-            rankings = predict_sequence(self.history)[
-                :, :, 0]  # update rankings
-
-            # map preds to content types
-            for i, key in enumerate(self.history.keys()):
-                self.ranking[key] = rankings[i]
+        # map preds to content types
+        for i, key in enumerate(self.history.keys()):
+            self.ranking[key] = rankings[i]
             
-            # reset stats
-            for key in self.history.keys():
-                self.history[key] = np.concatenate((self.history[key][self.pred_length:], np.zeros(self.pred_length)))
-
-        if self.timestep == self.bootstrap_period:  # end bootstrap ?
-            self.bootstrapping = False
+        # reset stats
+        for key in self.history.keys():
+            self.history[key] = np.zeros((self.window_length))
 
     def get_helper(self, item):
         try:
+            self.req_counter += 1
+            
+            if self.req_counter != 1 and (self.req_counter-1) % (self.reqs_per_day * self.window_length) == 0:
+                # if first update, copy over cache from bootstrap
+                if self.req_counter - 1 == self.reqs_per_day * self.window_length:
+                    self.store = self.bootstrap.store
+                # print('start updating rankings')
+                self.update_rankings()
+                # print('finished updating rankings')
             if item not in self.history:
                 self.history[item] = np.zeros(self.window_length)
             if item not in self.ranking:
                 self.ranking[item] = np.zeros(self.pred_length)
-            timestep = self.req_counter // self.reqs_per_day % self.window_length
-            self.history[item][timestep] += 1
+            # update history
+            self.history[item][self.get_curr_timestep()] += 1
             if self.bootstrapping:
                 cached_item = self.bootstrap.get(item)
             else:
@@ -182,17 +201,14 @@ class PretrainedCNNContentStore(ContentStore):
         except:
             return None
 
-    def predict_sequence(input_sequence):
+    def predict_sequence(self, input_sequence):
         history_sequence = input_sequence.copy()
         # initialize output (pred_steps time steps)
-        pred_sequence = np.zeros(
-            (input_sequence.shape[0], self.pred_length, self.num_features))
+        pred_sequence = np.zeros((input_sequence.shape[0], self.pred_length, self.num_features))
         for i in range(self.pred_length):
             # record next time step prediction (last time step of model output)
-            last_step_pred = self.model.predict(history_sequence)[
-                :, -1, :self.num_features]
+            last_step_pred = self.model.predict(history_sequence)[:, -1, :self.num_features]
             pred_sequence[:, i, :self.num_features] = last_step_pred
             # add the next time step prediction to the history sequence
-            history_sequence = np.concatenate(
-                [history_sequence, last_step_pred.reshape(-1, 1, self.num_features)], axis=1)
+            history_sequence = np.concatenate([history_sequence, last_step_pred.reshape(-1, 1, self.num_features)], axis=1)
         return pred_sequence
--- c_test/protocol.py
+++ c_test/protocol.py
@@ -31,8 +31,8 @@ def reply_replace_cache(fifo_send, victim_server, victim_page):
     fifo_send.flush()
 
 
-def worker(capacity, fifo_recv_path, fifo_send_path, model):
-    cache = cs(capacity, model)
+def worker(capacity, fifo_recv_path, fifo_send_path):
+    cache = cs(capacity)
 
     try:
         if not stat.S_ISFIFO(os.stat(fifo_recv_path).st_mode):
@@ -93,11 +93,11 @@ def main():
     parser.add_argument('capacity', type=str, help='capacity of content store')
     parser.add_argument('fifo_recv', type=str, help='path to read FIFO')
     parser.add_argument('fifo_send', type=str, help='path to write FIFO')
-    parser.add_argument('model', type=str, help='path to trained model')
     args = parser.parse_args()
 
     worker(args.capacity, args.fifo_recv,
-           args.fifo_send, args.capacity, args.model)
+           args.fifo_send, args.capacity)
+
 
 if __name__ == '__main__':
     main()
