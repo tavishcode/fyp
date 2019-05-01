49f6412b4f6b1a5ee151a7af17c2b8b6438d9e37
kelvin <kchiu@gmx.com>
2019-05-02 00:25:11 +0800

    Added item tracking for multiple servers

--- c_test/protocol.py
+++ c_test/protocol.py
@@ -78,20 +78,21 @@ def worker(capacity, fifo_recv_path, fifo_send_path):
             # print(f'Recv Interest: {day} {server} {page}') # assumes day starts at 0
 
             # Records statistics
-            cache.update_stats(day, page)
-            cache.get(page)
+            cache.update_stats(day, (server, page))
+            cache.get((server, page))
 
         elif message_type == 'D':
             # Received data
             # print(f'Recv Data: {server} {page}')
 
-            should_cache, victim = cache.add(page)
+            should_cache, victim = cache.add((server, page))
 
             if should_cache:  # Cache content ?
                 if victim == None:  # Cache without replacement ?
                     reply_cache(fifo_send)
                 else:
-                    reply_replace_cache(fifo_send, server, victim)
+                    victim_server, victim_page = victim
+                    reply_replace_cache(fifo_send, victim_server, victim_page)
             else:
                 reply_nocache(fifo_send)
 
